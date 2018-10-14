84187e808a3016c7f95f58b523610bc941b00ddf
grant smith <grantsmith1256@gmail.com>
2018-10-14 19:58:08 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,11 +4,12 @@ def reverse_each_word(sentence)
     word.reverse
     array << word.reverse
   end
-  sentence.join(" ")
+  sentence.join("")
 end
 
 def reverse_each_word(sentence)
 sentence.split(" ").collect do |word|
   word.reverse
 end
+sentence.join("")
 end
